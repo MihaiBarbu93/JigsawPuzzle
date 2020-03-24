03515405147305dc47b21ce2d0568584d04e7a63
Gheboianu Bogdan <bogdan.gheboianu.2013@gmail.com>
2020-03-25 01:08:48 +0200

    Removed commented section

--- static/js/game.js
+++ static/js/game.js
@@ -38,7 +38,7 @@ function getImage() {
 getImage();
 choseDifficulty()
 
-
+//=============================================================== TESTING =======================================================//
 var draggable = null
 
 
@@ -147,38 +147,3 @@ function dragAndDrop(draggable) {
 //     }}
 
 // draw(2,2)
-
-
-
-// POSSIBLE WAY TO DRAG AND DROP
-
-// function drag_start(event) {
-//     var style = window.getComputedStyle(event.target, null);
-//     event.dataTransfer.setData("text/plain",
-//     (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
-// } 
-
-
-// function drop(event) {
-//     var offset = event.dataTransfer.getData("text/plain").split(',');
-//     var pieces = document.querySelectorAll('#piece')
-//     for(p of pieces){
-//         p.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
-//         p.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
-//         event.preventDefault();
-//         return false;
-//     }
-
-// }
-
-// function drag_over(event) {
-//     event.preventDefault();
-//     return false;
-// } 
-
-// var pieces = document.querySelectorAll('#piece');
-// for(p of pieces){
-//     p.addEventListener('dragstart',drag_start,false);
-//     document.body.addEventListener('dragover',drag_over,false);
-//     document.body.addEventListener('drop',drop,false); 
-// }
