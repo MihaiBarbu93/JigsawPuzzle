6e6c48f6c90c57f7c938feefc7fd864cbc02efbb
Gheboianu Bogdan <bogdan.gheboianu.2013@gmail.com>
2020-03-24 21:29:30 +0200

    Added: save pieces of cropped image function

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, redirect, render_template
 from flask_ngrok import run_with_ngrok
+import sys
+sys.path.append('D:/Codecool/JigsawPuzzle/static/images')
+import crop_image as crp
 
 app = Flask(__name__)
 
@@ -22,6 +25,8 @@ def settings(category):
 
 @app.route('/game/<image>')
 def game(image):
+    image_path = f'static/images/{image}.jpg'
+    crp.cropImage(image_path, [2, 2])
     return render_template('game.html', image=image)
     
 
--- static/images/crop_image.py
+++ static/images/crop_image.py
@@ -1,29 +1,50 @@
 from PIL import Image
+import os
 
-# Open image
-image = Image.open('D:/Codecool/tic-tac-toe-javascript-BogdanGheboianu/static/images/neonimage1.jpg')
-# Get image dimensions
-width, height = image.size
-# Set puzzle dimensions
-puzzle_dimension = [4, 4]
-PDX = puzzle_dimension[0]
-PDY = puzzle_dimension[1]
-# Set piece of puzzle dimensions (-5px for safety)
-piece_width = round(width / PDX) - 5 
-piece_height = round(height / PDY) - 5
+def cropImage(image_path, puzzle_dimensions):
+    # Create folder for the pieces
+    folder_path = create_folder(image_path)
+    # Open image
+    image = Image.open(image_path)
+    # Get image dimensions
+    width, height = image.size
+    PDX = puzzle_dimensions[0]
+    PDY = puzzle_dimensions[1]
+    # Set piece of puzzle dimensions (-5px for safety)
+    piece_width = round(width / PDX) - 5 
+    piece_height = round(height / PDY) - 5
+    # Define start and end Y
+    startY = 0
+    endY = startY + piece_height
+    # Iterate through rows and columns and crop image
+    piece_id = 0
+    for row in range(PDX):
+        startX = 0
+        endX = startX + piece_width
+        for col in range(PDY):
+            cropped_piece = image.crop((startX, startY, endX, endY))
+            save_pieces(cropped_piece, piece_id, folder_path)
+            startX = startX + piece_width
+            endX = endX + piece_width
+            piece_id += 1
+        startY = startY + piece_height
+        endY = endY + piece_height
 
-startY = 0
-endY = startY + piece_height
 
-for row in range(PDX):
-    startX = 0
-    endX = startX + piece_width
-    for col in range(PDY):
-        cropped_piece = image.crop((startX, startY, endX, endY))
-        startX = startX + piece_width
-        endX = endX + piece_width
-        cropped_piece.show()
-    startY = startY + piece_height
-    endY = endY + piece_height
+def save_pieces(cropped_piece, piece_id, folder_path):
+    cropped_piece.save(f'{folder_path}\{piece_id}.jpeg', 'jpeg')
 
-# cropped_image.save('C:/users/bogda/Desktop/crop.jpeg', 'jpeg')
+
+def create_folder(image_path):
+    IMG_PATH = 0
+    IMG_NAME = -1
+    folder_name = image_path.split('.')[IMG_PATH].split('/')[IMG_NAME].replace('\\', '')
+    pieces_folder_path = f'{folder_name}_pieces'
+    try:
+        os.mkdir(pieces_folder_path)
+    except OSError:
+        print(f"Creation of the directory {pieces_folder_path} failed")
+    else:
+        return pieces_folder_path
+
+
