9b9eb4af89526e498960517c5ad6a56d75fea7e3
MihaiBarbu93 <mihai.barbu1993@gmail.com>
2020-03-26 09:48:28 +0200

    Merge branch 'master' of https://github.com/BogdanGheboianu/JigsawPuzzle
    # Conflicts:
    #	static/js/game.js

--- static/images/crop_image.py
+++ static/images/crop_image.py
@@ -5,8 +5,9 @@ import shutil
 
 image_info = {}
 # working_path='/home/alex/Documents/WEB MODULE 2/JIGSAW/JigsawPuzzle/static'
-working_path = "D:\Codecool\JigsawPuzzle\static"
+# working_path = "D:\Codecool\JigsawPuzzle\static"
 # working_path = "/Users/Silviu/Documents/Codecool/web_module/TW4_web/JigsawPuzzle/static"
+working_path = "/home/www/Desktop/webModule/TWW4/JigsawPuzzle/static"
 
 
 def cropImage(image_path, puzzle_dimensions):
